9c143fa1f8896fcd3607d0bc6cfd6751b5aa5143
Jason Farrell <imjason@gmail.com>
2024-08-01 15:39:02 -0400

    added batch support changes

--- PdfExtract/function_app.py
+++ PdfExtract/function_app.py
@@ -15,30 +15,37 @@ def download_pdf(req: func.HttpRequest) -> func.HttpResponse:
     req_body_json = req.get_json()
     print(req_body_json)
 
-    congress_number = req_body_json['values'][0]['data']['congressNumber']
-    bill_number = req_body_json['values'][0]['data']['billNumber']
-
-    if not congress_number or not bill_number:
-      return func.HttpResponse("Please provide congress number and bill name", status_code=400)
-
+    values = req_body_json.get('values')
+    if not values:
+      return func.HttpResponse("Please provide values", status_code=400)
+    
     blob_service_client = BlobServiceClient.from_connection_string(os.getenv('STORAGE_ACCOUNT_CONNECTION_STRING'))
-    blob_client = blob_service_client.get_blob_client(container='bills-pdf', blob=f'{congress_number}hr{bill_number}.pdf')
-    pdf_data = blob_client.download_blob().readall()
+    records = []
+    for record in values:
+      record_id = record['recordId']
+      congress_number = record['data']['congressNumber']
+      bill_number = record['data']['billNumber']
+    
+      if not congress_number or not bill_number:
+        return func.HttpResponse(f"Record: {record_id} does not contain either congress number or bill name", status_code=400)
+      
+      blob_client = blob_service_client.get_blob_client(container='bills-pdf', blob=f'{congress_number}hr{bill_number}.pdf')
+      pdf_data = blob_client.download_blob().readall()
+      pdf_reader = PyPDF2.PdfReader(io.BytesIO(pdf_data))
 
-    pdf_reader = PyPDF2.PdfReader(io.BytesIO(pdf_data))
-    text = ""
-    for page in pdf_reader.pages:
-      text += page.extract_text()
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
 
-    response = {
-      "values": [
+      records.append(
         {
-          "recordId": req_body_json['values'][0]['recordId'],
+          "recordId": record_id,
           "data": {
             "text_contents": text
           }
         }
-      ]
-    }
+      )
     
-    return func.HttpResponse(body=json.dumps(response), mimetype="application/json")
+    response_json = json.dumps({ "values": records })
+    print(response_json)
+    return func.HttpResponse(body=response_json, mimetype="application/json")
